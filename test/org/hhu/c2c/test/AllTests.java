6f82c1dddeeb18b413fc7478b1ed2d79f9eb6407
Oliver Schrenk <oliver.schrenk@uni-duesseldorf.de>
2009-09-17 06:49:57 +0000

    Added JUNIT4 test suite

---
+++ test/org/hhu/c2c/test/AllTests.java
@@ -0,0 +1,24 @@
+package org.hhu.c2c.test;
+
+import org.hhu.c2c.openlr.core.BearingTest;
+import org.hhu.c2c.openlr.core.DecoderTest;
+import org.hhu.c2c.openlr.core.DistanceTest;
+import org.hhu.c2c.openlr.core.EncoderTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Runs all tests.
+ * 
+ * @author Oliver Schrenk <oliver.schrenk@uni-duesseldorf.de>
+ * @version %I%, %G%
+ * 
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ BearingTest.class, DecoderTest.class, DistanceTest.class,
+		EncoderTest.class
+
+})
+public class AllTests {
+	// intentionally left empty
+}
